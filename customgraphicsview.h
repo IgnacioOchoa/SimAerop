de8eb89fbe3c9590c3a5a50e228cae00c2364005
IgnacioOchoa <onacho87@gmail.com>
2021-04-19 17:14:05 -0300

    Se implementó el scrolling
    Se incluyó la funcionalidad para poder scrollear lateralmente, y se
    mejoró un poco el zoom, para que el punto que estaba bajo el cursor antes
    del zoom, quede debajo del cursor después del zoom. Es la forma más
    cómoda de hacer zoom sobre un punto fijo.

--- customgraphicsview.h
+++ customgraphicsview.h
@@ -13,6 +13,15 @@ public:
     customGraphicsView(QWidget* w = nullptr);
 
     virtual void wheelEvent(QWheelEvent *event) override;
+    virtual void mouseMoveEvent(QMouseEvent *event) override;
+    virtual void mousePressEvent(QMouseEvent *event) override;
+    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+
+
+private:
+    bool ruedaApretada;
+    QPointF puntoInicioRueda;
+    QPointF centroAnterior;
 
 };
 
